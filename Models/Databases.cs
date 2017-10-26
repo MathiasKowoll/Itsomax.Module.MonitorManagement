862e8b6c887d7d675bd13dc52863923100765ece
Mathias Kowoll <mathias.kowoll@itsomax.com>
2017-10-26 17:48:48 -0300

    Added to self nuget packages

--- Models/Databases.cs
+++
@@ -1,9 +0,0 @@
-﻿using Itsomax.Data.Infrastructure.Models;
-using System.ComponentModel.DataAnnotations;
-
-namespace Itsomax.Module.MonitorManagement.Models
-{
-    class Databases :EntityBase
-    {
-    }
-}
